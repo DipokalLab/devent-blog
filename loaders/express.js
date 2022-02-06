8c2511e5e5ff8bb1b34097a501bfb1bb2ea03769
Dipokal HHJ <hhj@devent.kr>
2022-02-06 13:52:15 +0900

    chore: security module "helmet" added
    + 로더에 헬멧 보안 모듈 추가

--- loaders/express.js
+++ loaders/express.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import { engine } from 'express-handlebars';
-
+import * as helmet from "helmet";
 
 import apiRouter from '../backend/api.js';
 import mainRouter from '../frontend/routes/main.js';
@@ -18,7 +18,12 @@ export async function init (app) {
     app.set("view engine", "hbs");    
     app.set('views','./frontend/views');
     app.disable('x-powered-by');
-    
+
+    app.use(helmet.hidePoweredBy());
+    app.use(helmet.noSniff());
+    app.use(helmet.originAgentCluster());
+    app.use(helmet.referrerPolicy());
+    app.use(helmet.xssFilter());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended : true}));
     app.use(cookieParser());
