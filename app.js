457ecdfc733d6bcd6fc09c6bd88dddd43ddc3741
Dipokal HHJ <hhj@devent.kr>
2022-03-15 18:19:21 +0900

    build: added env
    + ENV 추가

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ import express from 'express';
 
 import * as loaderExpress from './loaders/express.js';
 
+if(process.env.NODE_ENV === 'prod') {
+    console.log('[ + ] Production mode');
+} else {
+    console.log('[ + ] Development mode');
+}
 
 async function startExpressServer() {
     const app = express();
-  
     await loaderExpress.init(app);
-  
+
     app.listen(9000, err => {
         console.log(`[ + ] The server is running.`);
     });
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,25 @@
-export default 
-{
-    "username": "roots",
-    "password": "testtesttest",
-    "host": "localhost",
-    "port": 3306,
-    "database": "dvt_blog"
+import Dotenv from 'dotenv';
+let config;
+
+if(process.env.NODE_ENV === 'prod') {
+    config = {
+        "username": process.env.MYSQL_USERNAME,
+        "password": process.env.MYSQL_PASSWORD,
+        "host": process.env.MYSQL_HOSTNAME,
+        "port": process.env.MYSQL_PORT,
+        "database": "dvt_blog"
+    }
+} else {
+    Dotenv.config({path: './.env'});
+
+    config = {
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASS,
+        "host": process.env.DB_HOST,
+        "port": process.env.DB_PORT,
+        "database": "dvt_blog"
+    }
 }
+
+
+export default config
